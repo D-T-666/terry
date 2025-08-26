c6f1f6c39f5d0e39f4bb2cca3e09bc104642d163
D-T-666 <tabatadzedima@protonmail.com>
2025-08-26 19:04:35 +0400

    added test title to the player

--- player/script.ts
+++ player/script.ts
@@ -2,20 +2,23 @@ import pages from "./modules/pages.ts";
 import { getTestContent } from "../scripts/api.ts";
 import "../util/browser-sim/browser-sim.js";
 import "./styles.css";
+import "../styles.css";
 import "../player.css";
 import { loadFile } from "../scripts/file-manager.ts";
 
 const mainContent = document.getElementById("main-content")!;
 const pageControls = document.getElementById("page-controls")!;
+const testNameElement = document.getElementById("test-name")!;
 
 const paramsString = window.location.search;
 const searchParams = new URLSearchParams(paramsString);
 const currentTestId = searchParams.get("id");
 
 if (currentTestId !== undefined) {
-	const { content } = await loadFile(currentTestId!);
+	const { name, content } = await loadFile(currentTestId!);
 
 	mainContent.innerHTML = content;
+	testNameElement.textContent = name!;
 
 	pages.init({
 		total: (mainContent.firstChild as HTMLElement).dataset.pages,
